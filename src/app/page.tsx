462c30bf62344f316795280bd0643741ff58d072
chaearn <chaearn@gmail.com>
2025-04-22 22:45:50 +0700

    move addUser to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,147 +1,75 @@
 'use client';
+import { useState } from 'react';
+import { supabase } from '@utils/supabase/client';
 import { useRouter } from 'next/navigation';
-import Image from "next/image";
 
-
-
-export default function Home() {
-  
+const AddUser: React.FC = () => {
+  const [name, setName] = useState<string>('');
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
   const router = useRouter()
-  
-  
+  const handleAddUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    setSuccess(null);
+
+    const { data, error } = await supabase.from('users').insert([{ name }]);
+
+    if (data) {
+        console.log('Inserted row:', data);
+      }
+      
+    if (error) {
+      setError(error.message);
+    } else {
+      setSuccess('User added successfully!');
+      setName('');
+      
+      router.push('/login')
+    }
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
         <div style={{ padding: '2rem', maxWidth: 400 }}>
-          <h1>ยินดีที่ได้รู้จัก 🌱</h1>
-          <p>อยากให้ Emmo เรียกแกว่าอะไรดี?</p>
-        
-          <input
-            type="text"
-            // value={name}
-            // onChange={(e) => setName(e.target.value)}
-            placeholder="เช่น: เอิร์น"
-            style={{
-              padding: '0.5rem',
-              fontSize: '1rem',
-              width: '100%',
-              marginBottom: '1rem',
-              borderRadius: 8,
-              border: '1px solid #ccc',
-            }}
-          />
-
-          <button
-            onClick={() => router.push('/login')}
-            // disabled={loading}
-            style={{
-              padding: '0.75rem',
-              width: '100%',
-              fontSize: '1rem',
-              borderRadius: 8,
-              backgroundColor: '#111827',
-              color: '#fff',
-            }}
-          >
-            Next
-           {/* {loading ? 'กำลังบันทึก...' : 'ถัดไป'} */}
-          </button>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: '2rem'}}>
+                <h1 style={{ fontWeight: 'bold', fontSize: '2rem'}}>ยินดีที่ได้รู้จัก 🌱</h1>
+                <p>ตอนคุยกันให้เราเรียกเธอว่าอะไรดี?</p>    
+            </div>
+            <form onSubmit={handleAddUser}>
+                <input
+                type="text"
+                placeholder="แนะนำตัวหน่อย"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                style={{
+                    padding: '0.5rem',
+                    fontSize: '1rem',
+                    width: '100%',
+                    marginBottom: '1rem',
+                    borderRadius: 8,
+                    border: '1px solid #ccc',
+                    textAlign: 'center'
+                }}
+                />
+                <button type="submit"
+                style={{
+                    padding: '0.75rem',
+                    width: '100%',
+                    fontSize: '1rem',
+                    borderRadius: 8,
+                    backgroundColor: '#111827',
+                    color: '#fff',
+                    }}>Next</button>
+            </form>
+            {error && <p style={{ color: 'red', textAlign: 'center', marginTop: '1rem' }}>{error}</p>}
+            {success && <p style={{ color: 'green', textAlign: 'center', marginTop: '1rem' }}>{success}</p>}
         </div>
-
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
   );
-}
+};
+
+export default AddUser;
