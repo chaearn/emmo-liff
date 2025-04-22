5ac21d386749847353c9b8e063607a258c79ab96
chaearn <chaearn@gmail.com>
2025-04-22 20:40:52 +0700

    debug button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import Image from "next/image";
+
 
 export default function Home() {
   
-
+  const router = useRouter()
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -34,7 +38,7 @@ export default function Home() {
           />
 
           <button
-            // onClick={relocateLogin} 
+            onClick={() => router.push('/login')}
             // disabled={loading}
             style={{
               padding: '0.75rem',
